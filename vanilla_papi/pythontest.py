8bf845cbf2e45c9d98e3bce055e0d6ba96214f87
Andre Malcher <am4491@columbia.edu>
2016-02-15 13:38:23 -0600

    Changed gitignore to ignore hidden files, optimization reports and *.data files generated by setrun.py

---
+++ vanilla_papi/pythontest.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import numpy
+import sys
+import clawpack.pyclaw.solution as solution
+import clawpack.pyclaw.controller as controller
+
+# Compute error norm for and maximum
+def compute_error(q, qref, n, name):
+    # TODO: Check if this is working as expected
+    error = numpy.linalg.norm(q - qref, ord=n)
+#    print "\tError ({:d}-norm): {:e}".format(n, error)
+#    print "\t%lf" % (error)
+    maxdiff = numpy.max(abs(q-qref))
+#    print "\tMax difference: {:e}".format(maxdiff)
+#    print maxdiff
+    return [error, maxdiff]
+
+if __name__ == '__main__':
+    print "Comparison of \"" + sys.argv[1] + "\" and \"" + sys.argv[2] + "\""
+
+    # Load reference solution and test solution
+    #n = numpy.inf # n-norm
+    n = 1.0 # n-norm
+
+    # Lists to store errors (p-norm) and absolute difference, for h, hu, hv
+    errors = [[],[],[]]
+
+    tbegin = 0
+    tend =19
+    print "### Computing error ({:f}-norm) and maximum difference for time step {:d} to {:d} ###".format(n, tbegin, tend)
+    for t in xrange(tbegin,tend):
+        #ref = solution.Solution(t, path="./orig_ipo", read_aux=False)
+        #sol = solution.Solution(t, path="./orig_precise", read_aux=False)
+        ref = solution.Solution(t, path=sys.argv[1], read_aux=False)
+        sol = solution.Solution(t, path=sys.argv[2], read_aux=False)
+#        print "=== Test solution stats for timestep {:d} ===".format(t)
+#        print "Number of cells: " + str(ref.q.shape[0])
+#        print "Number of cells (x-dir): " + str(ref.q.shape[1])
+#        print "Number of cells (y-dir): " + str(ref.q.shape[2])
+
+        # Format: h = h(x, t)
+        h     = sol.q[0, ...]
+        href  = ref.q[0, ...]
+        hu    = sol.q[1, ...]
+        huref = ref.q[1, ...]
+        hv    = sol.q[2, ...]
+        hvref = ref.q[2, ...]
+        
+        #if (t==7):
+        #    print h
+        errors[0].append(compute_error(h,  href,  n, "h"))
+        errors[1].append(compute_error(hu, huref, n, "hu"))
+        errors[2].append(compute_error(hv, hvref, n, "hv"))
+#        print "============================================="
+#    print "\n".join(str(a) for a in herr)
+
+    # STATS
+    for q in errors:
+        maxima = [(max(a)) for a in zip(*q)]
+        print "===> Stats for ???TODO???:" # Add h, hu, hv
+        print "  Maximum {:f}-norm found:              {:e}".format(n, maxima[0])
+        print "  Maximum absolute difference found: {:e}".format(maxima[1])
+    
