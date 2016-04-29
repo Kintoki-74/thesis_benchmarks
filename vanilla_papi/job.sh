d9c222437d9b94b8f62b08ab6c3e52e24107cce8
Andre Malcher <am4491@columbia.edu>
2016-04-29 02:41:04 -0500

    Updated Makefile to use -DUSEPAPI and local copy of geoclaw_riemann_tools.f and updated job.sh to use normal nodes and only send email upon failure

--- vanilla_papi/job.sh
+++ vanilla_papi/job.sh
@@ -4,11 +4,10 @@
 #SBATCH -e CHILE2010_ERR_%j # error file name (%j expands to jobID)
 #SBATCH -N 1                # total number of nodes requested (16 cores/node)
 #SBATCH -n 1                # 1 task
-#SBATCH -p development      # queue (partition) -- serial, normal, development, etc.
-#SBATCH -t 00:05:00         # run time (hh:mm:ss)
+#SBATCH -p normal           # queue (partition) -- serial, normal, development, etc.
+#SBATCH -t 02:00:00         # run time (hh:mm:ss)
 #SBATCH --mail-user=andreemalcher@gmail.com
-#SBATCH --mail-type=begin   # email me when the job starts
-#SBATCH --mail-type=end     # email me when the job finishes
+#SBATCH --mail-type=fail    # email me when the job starts
 
 make topo
 make data
